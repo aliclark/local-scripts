ed54cae2522362df4738658771a757fa50885b49
Ali Clark <ali@clark.gb.net>
2012-09-08 11:08:18 +0100

    vim script should run vi if vim not found

--- vim.sh
+++ vim.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 
+editor=vi
+
+# Must be checking at /usr/bin not /usr/local/bin
+[ -x /usr/bin/vim ] && editor=vim
+
 if [ "$TERM" = "screen" ]; then
-    /usr/bin/vim $@
+    /usr/bin/$editor $@
 elif [ "$TERM" = "xterm" ]; then
-    /usr/bin/vim $@
+    /usr/bin/$editor $@
 elif [ -n "$DISPLAY" ]; then
-    xterm -e /usr/bin/vim $@
+    xterm -e /usr/bin/$editor $@
 else
     # change this to gvim
-    /usr/bin/vim $@
+    /usr/bin/$editor $@
 fi
 
